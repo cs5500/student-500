e871a9170bc3e245da984ba4d90d6d627d40a75f
Vaibhav Rangarajan <rangarajan.v@husky.neu.edu>
2018-04-19 16:56:55 -0400

    Added

--- hmk1/two.py
+++
@@ -1,6 +0,0 @@
-#subtracts values of a and b
-def diff(a, b):
-    return a + b
-
-#return sum
-print("The sum is %i" % (6, 8, diff(6, 8)))
--- hmk2/studentC.py
+++
@@ -1,4 +0,0 @@
-def diff(a, b):
-    c=a + b
-    return c
-print("The sum is %i" % (6, 8, diff(6, 8)))
